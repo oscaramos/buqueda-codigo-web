a5d5e540bdca78ae2ff89a1eb5b4b6dede57ac9f
Oscar <oscar.ramos@ucsp.edu.pe>
2020-07-04 01:14:35 -0500

    Appbar completed

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
 import React, { useState, useEffect } from 'react'
-import searchCode from './api/api'
+
 import Card from '@material-ui/core/Card'
+import AppBar from '@material-ui/core/AppBar'
+import TextField from '@material-ui/core/TextField'
 import CardContent from '@material-ui/core/CardContent'
 import Grid from '@material-ui/core/Grid'
-import { makeStyles } from '@material-ui/core/styles'
 import Container from '@material-ui/core/Container'
-import TextField from '@material-ui/core/TextField'
+import IconButton from '@material-ui/core/IconButton'
+import InputAdornment from '@material-ui/core/InputAdornment'
+import { makeStyles } from '@material-ui/core/styles'
+
+import searchCode from './api/api'
+
+import { ReactComponent as SearchIcon } from './icons/search.svg'
 
 import './App.css'
 
-const useStyles = makeStyles(() => ({
+const useStyles = makeStyles((theme) => ({
 	'@global': {
 		html: {
 			backgroundColor: '#eeeeef',
@@ -21,27 +28,43 @@ const useStyles = makeStyles(() => ({
 		maxWidth: '600px',
 	},
 	searchBar: {
-		width: '100%',
 		borderRadius: '6px',
 		border: '1px solid #D7DAE0',
 		backgroundColor: '#fff',
+		width: '35em',
+		margin: 'auto',
+	},
+	appbar: {
+		height: '6em',
+		backgroundColor: '#fff',
+		boxShadow: 'none',
 	}
 }))
 
 const useStylesSearch = makeStyles(() => ({
 	root: {
 		fontFamily: 'Menlo',
+	},
+	icon: {
+		width: '1.5em'
 	}
 }))
 
 function SearchBar(props) {
 	const classes = useStylesSearch()
-	return <TextField
-		variant='outlined'
-		InputProps={{classes}}
-		placeholder='Search for java code'
-		{...props}
-	/>
+	return (
+		<React.Fragment>
+			<TextField
+				variant='outlined'
+				InputProps={{classes,
+					endAdornment:
+						<InputAdornment position="end">
+							<SearchIcon className={classes.icon} />
+						</InputAdornment> }}
+				placeholder='Search for java code'
+				{...props}
+			/>
+		</React.Fragment>)
 }
 
 function App() {
@@ -56,22 +79,26 @@ function App() {
 	}, [])
 
 	return (
-		<Container maxWidth="sm">
-			<SearchBar className={classes.searchBar}/>
-
-			<Grid container spacing={3}>
-				{codes.map((code, index) => (
-					<Grid item key={index} className={classes.cardContainer}>
-						<Card>
-							<CardContent>
-								{code}
-							</CardContent>
-						</Card>
-					</Grid>
-				))}
-			</Grid>
-
-		</Container>
+		<div>
+			<div>
+				<AppBar position='fixed' className={classes.appbar}>
+					<SearchBar className={classes.searchBar}/>
+				</AppBar>
+			</div>
+			<Container maxWidth="sm">
+				<Grid container spacing={3}>
+					{codes.map((code, index) => (
+						<Grid item key={index} className={classes.cardContainer}>
+							<Card>
+								<CardContent>
+									{code}
+								</CardContent>
+							</Card>
+						</Grid>
+					))}
+				</Grid>
+			</Container>
+		</div>
 	)
 }
 
